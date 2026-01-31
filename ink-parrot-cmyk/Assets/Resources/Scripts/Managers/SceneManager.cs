32f87f4bbf9ec4e3c638dedd78ed52ba9f3eb60b
heohuhu <nicejunmin@daum.net>
2026-01-31 23:41:20 +0900

    +TMP, StartMenu UI

--- ink-parrot-cmyk/Assets/Resources/Scripts/Managers/SceneManager.cs
+++ ink-parrot-cmyk/Assets/Resources/Scripts/Managers/SceneManager.cs
@@ -1,16 +1,133 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SceneController Instance;
+
+    [Header("Fade Option")]
+    [SerializeField] private CanvasGroup fadeCanvas;
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    private void Awake()
     {
-        
+        // 싱글톤 처리
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        LoadSceneAdditive("StartMenu");
+    }
+
+    /* =====================
+     * Scene Load
+     * ===================== */
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsyncCoroutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsyncCoroutine(string sceneName)
+    {
+        yield return FadeOut();
+
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncOp.isDone)
+        {
+            yield return null;
+        }
+
+        yield return FadeIn();
+    }
+
+    public void LoadSceneAdditive(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    /* =====================
+     * Reload
+     * ===================== */
+
+    public void ReloadCurrentScene()
+    {
+        Scene current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.name);
+    }
+
+    /* =====================
+     * Fade
+     * ===================== */
+
+    private IEnumerator FadeOut()
+    {
+        if (fadeCanvas == null)
+            yield break;
+
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            fadeCanvas.alpha = Mathf.Lerp(0f, 1f, time / fadeDuration);
+            yield return null;
+        }
+
+        fadeCanvas.alpha = 1f;
+    }
+
+    private IEnumerator FadeIn()
+    {
+        if (fadeCanvas == null)
+            yield break;
+
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            fadeCanvas.alpha = Mathf.Lerp(1f, 0f, time / fadeDuration);
+            yield return null;
+        }
+
+        fadeCanvas.alpha = 0f;
+    }
+
+    /* =====================
+     * Utility
+     * ===================== */
+
+    public string GetCurrentSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public int GetCurrentSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
     }
 }
